c2449185549bba995268788e471bfe8e2c813a63
kicandray <kevin.candray.688@my.csun.edu>
2021-05-14 12:59:11 -0700

    updating script

--- Tidy5/scripts/ripceo.R
+++ Tidy5/scripts/ripceo.R
@@ -9,7 +9,27 @@ View(departures)
 #loading the libs 
 library(tidyverse)
 library(here)
-library(scales)
-
+library(lubridate)
+library(tidytext)
+library(flair)
+library(wordcloud2)
+library(DescTools)
+library(webshot)
+library("htmlwidgets")
 ripceo <- departures %>% 
   ggplot(aes(fyear))
+
+departures_words<- departures %>% 
+  unnest_tokens(word, notes) %>% 
+  anti_join(stop_words, by = "word") %>% 
+  count(coname, fyear, departure_code, word)
+
+departurewords<- departures_words %>% 
+  count(word) %>% 
+  arrange(desc(n)) %>% 
+  filter(n >=7) %>% 
+  slice(1:100)
+
+departure.cloud<- wordcloud2(departurewords, shape = 'star', size = 0.25, minSize = 5, rotateRatio = 0, shuffle = FALSE)
+saveWidget(departure.cloud, "dep.html", selfcontained = F)
+webshot("dep.html", "dep_1.png", delay = 5, vwidth = 480, vheight = 480)
